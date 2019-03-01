fff1c29a66b8eed8a2a3361ceb86a5f90ed9c705
samjeffcoat <samjeffcoat@gmail.com>
2019-03-01 10:57:19 -0600

    Sets up server files

--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const projectRouter = require("../routes/projects-router");
 const server = express();
 
 server.use(helmet());
 
 server.use(express.json());
 
-server.use("/api/projects");
+server.use("/api/projects", projectRouter);
 
 module.exports = server;
