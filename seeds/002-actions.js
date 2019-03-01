6012e744e760b3ba9bdcefc21fb626cb65e7fcff
samjeffcoat <samjeffcoat@gmail.com>
2019-03-01 12:43:33 -0600

    Adds action seed

--- seeds/002-actions.js
+++ seeds/002-actions.js
@@ -1,13 +1,16 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("actions")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("actions").insert([
+        {
+          project_id: 1,
+          description: "Complete BR Challenge",
+          notes: "Track Results, Profit",
+          is_complete: false
+        }
       ]);
     });
 };
