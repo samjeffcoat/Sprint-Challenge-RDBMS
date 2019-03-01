d9bc593ecd06289caf53c92caf0887f11cbf34e5
samjeffcoat <samjeffcoat@gmail.com>
2019-03-01 11:18:34 -0600

    adds the ability to add a project, tested in postman, successful

--- routes/projects-router.js
+++ routes/projects-router.js
@@ -6,4 +6,19 @@ const knexConfig = require("../knexfile");
 
 const db = knex(knexConfig.development);
 
+router.post("/", (req, res) => {
+  db("projects")
+    .insert(req.body)
+    .then(([id]) => {
+      db("projects")
+        .where({ id })
+        .first()
+        .then(bigbear => {
+          res.status(200).json(bigbear);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 module.exports = router;
