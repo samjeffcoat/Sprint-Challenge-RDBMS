18a7023c14f46b129e7d6452ff71e47b857aa014
samjeffcoat <samjeffcoat@gmail.com>
2019-03-01 12:29:25 -0600

    projects seed created

--- seeds/001-projects.js
+++ seeds/001-projects.js
@@ -1,13 +1,15 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("projects")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("projects").insert([
+        {
+          name: "Bankroll Challenge",
+          description: "10k by end of baseball",
+          is_complete: false
+        }
       ]);
     });
 };
